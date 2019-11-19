12bf071ccb112c777a267302e684799d79b4b314
SixthSun <sixthsunltd@gmail.com>
2019-11-19 12:07:11 +0000

    Added summary comments to models

--- TournamentTracker/TrackerLibrary/PersonModel.cs
+++ TournamentTracker/TrackerLibrary/PersonModel.cs
@@ -1,10 +1,28 @@
 ﻿namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// The first name of the person
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The primary email of the person
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The primary phone number of the person
+        /// </summary>
         public string PhoneNumber { get; set; }
     }
 }
