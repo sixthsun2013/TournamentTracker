b6d3c019684e2d2902a9b14c334766c5ea676dc8
SixthSun <sixthsunltd@gmail.com>
2019-11-19 11:51:30 +0000

    Added data models

---
+++ TournamentTracker/TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+        public int PlaceNumber { get; set; }
+        public int PlaceName { get; set; }
+        public int PrizeAmount { get; set; }
+        public int PrizePercentage { get; set; }
+    }
+}
